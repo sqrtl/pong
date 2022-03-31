b16df91883cc3e22349ab0a69c3c32420332022e
Brasil <squirtle.brazil@gmail.com>
2022-03-31 03:33:04 -0400

    initial goal / first commit

--- main.js
+++ main.js
@@ -11,10 +11,20 @@ const BALL_SIZE = 20;
 
 // Get the computer paddle element
 const computerPaddle = document.querySelector('.computer-paddle');
+// Other Elements (Player paddle and ball)
+const playerPaddle = document.querySelector('.player-paddle');
+const ball = document.querySelector('.ball');
+
+// Ball's x and y position and velocity
+let ballXPosition = 0;
+let ballYPosition = 0;
+let yVelocity = 5;
+let xVelocity = 4;
 
 // The y-velocity of the computer paddle
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
+
 
 // Update the pong world
 function update() {
@@ -22,8 +32,25 @@ function update() {
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
+    if (computerPaddleYPosition === 0 || computerPaddleYPosition === 400){
+        computerPaddleYVelocity *= -1;
+    }
+
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+    // Update the Ball's X and y axis.
+    ballXPosition += xVelocity;
+    ballYPosition += yVelocity;
+    if (ballXPosition === 0 || ballXPosition === 680){
+        xVelocity *= -1
+    }
+    if (ballYPosition === 0 || ballYPosition === 480){
+        yVelocity *= -1
+    }
+    ball.style.top = `${ballYPosition}px`;
+    ball.style.left = `${ballXPosition}px`;
+    
 }
 
 // Call the update() function every 35ms
